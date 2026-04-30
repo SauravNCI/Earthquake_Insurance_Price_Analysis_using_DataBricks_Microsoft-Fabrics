005d5707ce9f082b279c9e5c65d7e69de0505f2c
sauravnci <sauravchaudhary_bt18@iiitkalyani.ac.in>
2026-04-30 17:10:49 +0100

    first commit

---
+++ notebooks/DataWarehouse_Query.sql
@@ -0,0 +1,19 @@
+CREATE TABLE dbo.gold_analysis2_multihazard_profile AS
+SELECT *
+FROM Earthquake_Insurance_Lakehouse.dbo.gold_analysis2_multihazard_profile;
+
+CREATE TABLE dbo.gold_analysis2_eq_state_profile AS
+SELECT *
+FROM Earthquake_Insurance_Lakehouse.dbo.gold_analysis2_eq_state_profile;
+
+CREATE TABLE dbo.gold_analysis3_national_trend AS
+SELECT *
+FROM Earthquake_Insurance_Lakehouse.dbo.gold_analysis3_national_trend;
+
+CREATE TABLE dbo.gold_analysis3_annual_by_state AS
+SELECT *
+FROM Earthquake_Insurance_Lakehouse.dbo.gold_analysis3_annual_by_state;
+
+CREATE TABLE dbo.gold_summary_statistics AS
+SELECT *
+FROM Earthquake_Insurance_Lakehouse.dbo.gold_summary_statistics;
